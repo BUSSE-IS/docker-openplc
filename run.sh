ed38b9754defd275d5c278b1de30846034534cb6
jonrot1906 <rotter.jonas@gmail.com>
2020-10-11 02:51:29 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,8 +10,8 @@ if [ ! -f /home/openplc/scripts/mbconfig.cfg ]; then
     exit 1
 fi
 
-SQL_SCRIPT="INSERT INTO Programs (Name, Description, File, Date_upload) VALUES ('Test', 'Desc', 'script.st', strftime('%s', 'now'));"
-SQL_DEVICE="INSERT INTO Slave_dev (dev_name, dev_type, slave_id, ip_address, ip_port, di_start, di_size, coil_start, coil_size, ir_start, ir_size, hr_read_start, hr_read_size, hr_write_start, hr_write_size) VALUES ('Testdevice', 'TCP', 0, '127.0.0.1', 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);"
+SQL_SCRIPT="INSERT INTO Programs (Name, Description, File, Date_upload) VALUES ('Hauptprogramm', 'Dies ist das Hauptprogramm fuer diese BUSSEplc-Einheit.', 'script.st', strftime('%s', 'now'));"
+SQL_DEVICE="INSERT INTO Slave_dev (dev_name, dev_type, slave_id, com_port, baud_rate, data_bits, stop_bits, di_start, di_size, coil_start, coil_size, ir_start, ir_size, hr_read_start, hr_read_size, hr_write_start, hr_write_size) VALUES ('Arduino Mega', 'Mega', 1, '/dev/ttyAMA0', 115200, 8, 1, 0, 24, 0, 16, 0, 16, 0, 0, 0, 12);"
 SQL_AUTOST="UPDATE Settings SET Value = 'true' WHERE Key = 'Start_run_mode';"
 
 cp /home/openplc/scripts/script.st /home/openplc/OpenPLC_v3/webserver/st_files
